b4c9d2ccc9fa598ba93148b89dacb79d1dbd9974
Dilek ŞEKERCİ <89838610+dileksekerci@users.noreply.github.com>
2022-08-17 01:33:41 +0300

    js updated

--- JavaScript-Odev1-SaatveKarsilama/js/clock.js
+++ JavaScript-Odev1-SaatveKarsilama/js/clock.js
@@ -1,7 +1,7 @@
-//ekranda popap açılmasını sağlayan bölüm
+//ekranda popup çıkmasını sağlayan bölüm
 let fullName = prompt("Lütfen Adınızı Giriniz: ")
 
-//açılan popap ekrandan gelen değeri Html koda dönüştürerek gönderen bölüm
+//popup ekrandan gelen değeri HTML koda dönüştürerek gönderen bölüm
 let myName = document.querySelector("#myName")
 myName.innerHTML = `${myName.innerHTML} <strong>${fullName}</strong>`
 
