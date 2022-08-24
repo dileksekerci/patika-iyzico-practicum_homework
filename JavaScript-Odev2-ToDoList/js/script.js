934cabaf3937b818a488ecc3c0e5ddb168224d1b
Dilek ŞEKERCİ <89838610+dileksekerci@users.noreply.github.com>
2022-08-24 16:47:32 +0300

    index and styles completed, script updated

--- JavaScript-Odev2-ToDoList/js/script.js
+++ JavaScript-Odev2-ToDoList/js/script.js
@@ -1,68 +1,75 @@
-const form = document.querySelector("#new-todo")
-const todoList = document.querySelector("#todo-list")
-const todoText = document.querySelector("#todo-text")
+let toastBtn = document.querySelector("#toastButton")
+let todoList = document.querySelector("#todo-list")
+let todoText = document.querySelector("#text")
+let form = document.querySelector("#new-todo")
+let ullength = document.getElementsByTagName("li");
+
+let taskList = [];
+
+if (localStorage.getItem("taskList") !== null) {
+    taskList = JSON.parse(localStorage.getItem("taskList"));
+}
+
 
 form.addEventListener("submit", addTodo)
 
-todoList.addEventListener("click", done)
+//Choose an element
+function check() {
+    this.classList.toggle("checked");
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+}
 
-//Toast 
-//var empToast = new bootstrap.Toast(document.getElementById("toast-empty"));
-//var insToast = new bootstrap.Toast(document.getElementById("toast-ins"));
-//var delToast = new bootstrap.Toast(document.getElementById("toast-del"));
+function removeButton() {
+    this.parentElement.remove();
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+}
 
+//add new list item
+function newListItem() {
+    let todoLi = document.createElement('li')
+    todoList.appendChild(todoLi);
+    todoLi.innerHTML = text.value;
+    localStorage.setItem("taskList", JSON.stringify(taskList));
 
-//add new list item 
-function newListItem(todo) {
-    const todoLi = document.createElement("Li")
-    todoLi.classList = "d-flex todoListItem"
-    todoList.appendChild(todoLi)
-
-    //new list element
-    const todoText = document.createElement('p')
-    todoText.classList = "list-group-item text-left w-100 m-1 text-dark"
-    todoText.innerText = todo
-    todoLi.appendChild(todoText)
+    //chosee
+    todoLi.onclick = check;
 
     //create delete button
-    const deleteBtn = document.createElement("Button")
-    deleteBtn.classList = "btn btn-danger w-20 ms-auto h-100 my-auto"
-    deleteBtn.innerText = "X"
-    deleteBtn.onclick = deleteTodo
-    todoLi.appendChild(deleteBtn)
+    let deleteBtn = document.createElement("span");
+    deleteBtn.textContent = "\u00D7";
+    deleteBtn.classList.add("close");
+    deleteBtn.onclick = removeButton;
+
+    todoLi.append(deleteBtn);
+
 }
 
 
 //Check the added content
 function addTodo(event) {
     if (todoText.value.length === 0) {
-        //empToast.show()
-        event.preventDefault()
+        $(".toast-empty").toast("show");
+        event.preventDefault();
     } else {
-        newListItem(todoText.value)
-        todoText.value = ''
-        //insToast.show()
-        event.preventDefault()
+        newListItem(todoText.value);
+        todoText.value = '';
+        $(".toast-add").toast("show");
+        event.preventDefault();
     }
 }
 
 //Delete an element
 function deleteTodo() {
-    const todoText = this.parentElement.querySelector('p').innerText
-    const todoListElements = [...document.querySelectorAll(".todoListItem")]
-    todoListElements.filter(element => element.querySelector('p').innerText === todoText)
-        .forEach(element => element.remove())
-    delToast.show()
+    for (let i = 0; i < ullength.length; i++) {
+        let deleteBtn = document.createElement("span");
+        deleteBtn.textContent = "\u00D7";
+        deleteBtn.classList.add("close");
+        deleteBtn.onclick = removeButton;
+        ullength[i].append(deleteBtn);
+        ullength[i].onclick = check;
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+    }
 }
 
 
-//Choose an element
-function done() {
-    const todoText = this.parentElement.querySelector('p').innerText
-    const todoListElements = [...document.querySelectorAll(".todoListItem")]
-    todoListElements.filter(element => element.querySelector('p').innerText === todoText)
-        .forEach(element => element.classList.add("checked"))
-}
-
-
-//TODO: tost kullanımı - seçim yapma kaldırma - local storage
+//TODO: Local Storage Çalışmıyor
